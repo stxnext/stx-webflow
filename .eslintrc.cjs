91f8eac26b9d2fc7d5d5c01733cdbbce2a99086f
maximorloff <maxim.orlov@stxnext.pl>
2024-04-17 08:57:12 +0200

    wip: dev env config

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  root: true,
-  extends: ['plugin:prettier/recommended'],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-  }
-}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,42 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  ...compat.extends("standard-with-typescript"),
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends('standard-with-typescript'),
   ...tseslint.configs.recommended,
+  {
+    ignores: [
+      'public',
+      'dist',
+      'pnpm-lock.yaml',
+      'pnpm-workspace.yaml',
+      'eslint.config.mjs',
+    ]
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/semi': 'off',
+      '@typescript-eslint/comma-dangle': 'off',
+      '@typescript-eslint/member-delimiter-style': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off'
+    },
+  },
 ];
